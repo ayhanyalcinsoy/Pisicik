c538f1bffaa7a616c0df1aaedb4666b8a70e2f36
ayhanyalcinsoy <ayhanyalcinsoy@gmail.com>
2015-01-14 16:10:27 +0200

    shiboken:splitted to 3 packages

--- applications/shiboken/actions.py
+++ applications/shiboken/actions.py
@@ -10,24 +10,20 @@ from pisi.actionsapi import get
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import shelltools
 
-WorkDir = ".."
+WorkDir = "."
 
 def setup():
-    shelltools.cd(".")
-    shelltools.makedirs("build_python3")
-    shelltools.copytree("work/shiboken-1.2.2/", "build_python3")
     #python2
-    shelltools.cd("work/shiboken-1.2.2/")
-    shelltools.makedirs("build")
-    shelltools.cd("build")
+    shelltools.cd("shiboken-1.2.2/")
+    shelltools.makedirs("build2")
+    shelltools.makedirs("build3")
+    shelltools.cd("build2")
     cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr  \
                           -DCMAKE_INSTALL_SYSCONFDIR=/etc \
                           -DCMAKE_INSTALL_LIBDIR=/usr/lib", sourceDir="..")
 
     #python3
-    shelltools.cd(".../build_python3/")
-    shelltools.makedirs("build")
-    shelltools.cd("build")
+    shelltools.cd("../build3")
     cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr  \
                           -DCMAKE_INSTALL_SYSCONFDIR=/etc \
                           -DUSE_PYTHON3=yes \
@@ -36,20 +32,23 @@ def setup():
 
 def build():
     #python2
-    shelltools.cd("work/shiboken-1.2.2/")
+    shelltools.cd("shiboken-1.2.2/build2")
     autotools.make()
     #python3
-    shelltools.cd("build2")
+    shelltools.cd("../build3")
     autotools.make()
 def install():
     #Python2
-    shelltools.cd("build")
+    shelltools.cd("shiboken-1.2.2/build2")
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-    #Python3
-    shelltools.cd("build2")
-    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.removeDir("usr/bin")
-    pisitools.removeDir("usr/include")
-    pisitools.removeDir("usr/share")
+    pisitools.rename("usr/lib/pkgconfig/shiboken.pc", "shiboken-py2.pc")
     pisitools.remove("usr/lib/cmake/Shiboken-1.2.2/ShibokenConfigVersion.cmake")
     pisitools.remove("usr/lib/cmake/Shiboken-1.2.2/ShibokenConfig.cmake")
+    #Python3
+    shelltools.cd("../build3")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.rename("usr/lib/pkgconfig/shiboken.pc", "shiboken-py3.pc")
+    #pisitools.removeDir("usr/bin")
+    #pisitools.removeDir("usr/include")
+    #pisitools.removeDir("usr/share")
+
