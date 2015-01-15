76e6270e7bc13f02c6ce9ee3d77e91fadf4c068b
ayhanyalcinsoy <ayhanyalcinsoy@gmail.com>
2015-01-15 11:12:19 +0200

    shiboken:renamed

--- applications/python-shiboken/actions.py
+++ applications/python-shiboken/actions.py
@@ -10,23 +10,45 @@ from pisi.actionsapi import get
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import shelltools
 
-def setup():
-    shelltools.makedirs("build")
-    shelltools.cd("build")
+WorkDir = "."
 
+def setup():
+    #python2
+    shelltools.cd("shiboken-1.2.2/")
+    shelltools.makedirs("build2")
+    shelltools.makedirs("build3")
+    shelltools.cd("build2")
     cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr  \
                           -DCMAKE_INSTALL_SYSCONFDIR=/etc \
                           -DCMAKE_INSTALL_LIBDIR=/usr/lib", sourceDir="..")
 
-def build():
-    shelltools.cd("build")
-    autotools.make()
+    #python3
+    shelltools.cd("../build3")
+    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr  \
+                          -DCMAKE_INSTALL_SYSCONFDIR=/etc \
+                          -DUSE_PYTHON3=yes \
+                          -DBUILD_TESTS=OFF \
+                          -DCMAKE_INSTALL_LIBDIR=/usr/lib", sourceDir="..")
 
+def build():
+    #python2
+    shelltools.cd("shiboken-1.2.2/build2")
+    autotools.make()
+    #python3
+    shelltools.cd("../build3")
+    autotools.make()
 def install():
-    shelltools.cd("build")
+    #Python2
+    shelltools.cd("shiboken-1.2.2/build2")
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.removeDir("usr/bin")
-    pisitools.removeDir("usr/include")
-    pisitools.removeDir("usr/share")
+    pisitools.rename("usr/lib/pkgconfig/shiboken.pc", "shiboken-py2.pc")
     pisitools.remove("usr/lib/cmake/Shiboken-1.2.2/ShibokenConfigVersion.cmake")
     pisitools.remove("usr/lib/cmake/Shiboken-1.2.2/ShibokenConfig.cmake")
+    #Python3
+    shelltools.cd("../build3")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.rename("usr/lib/pkgconfig/shiboken.pc", "shiboken-py3.pc")
+    #pisitools.removeDir("usr/bin")
+    #pisitools.removeDir("usr/include")
+    #pisitools.removeDir("usr/share")
+
