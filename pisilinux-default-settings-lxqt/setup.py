8716b59b6b7c0d08fb1113622f2795a631861494
ayhanyalcinsoy <ayhanyalcinsoy@gmail.com>
2015-02-13 11:40:58 +0200

    modified for new lxqt

--- pisilinux-default-settings-lxqt/setup.py
+++ pisilinux-default-settings-lxqt/setup.py
@@ -5,13 +5,11 @@ import glob
 from distutils.core import setup
 
 datas = [
-    ('/usr/share/lxqt/themes/a-mego', glob.glob('a-mego/*')),
     ('/usr/share/lxqt/themes/ambiance', glob.glob('ambiance/*')),
-    ('/usr/share/lxqt/themes/flat', glob.glob('flat/*')),
-    ('/usr/share/lxqt/themes/flat-dark-alpha', glob.glob('flat-dark-alpha/*')),
+    ('/usr/share/lxqt/themes/frost', glob.glob('frost/*')),
+    ('/usr/share/lxqt/themes/dark', glob.glob('dark/*')),
     ('/usr/share/lxqt/themes/light', glob.glob('light/*')),
-    ('/usr/share/lxqt/themes/green', glob.glob('green/*')),
-    ('/usr/share/lxqt/themes/plasma-next-alpha', glob.glob('plasma-next-alpha/*')),
+    ('/usr/share/lxqt/themes/kde-plasma', glob.glob('kde-plasma/*')),
     ('/usr/share/lxdm/themes/lxdm-pisilinux-theme', glob.glob('lxdm/login.png')),
     ('/etc/lxqt/', glob.glob('lxqt/*')),
     ('/etc/pcmanfm-qt/lxqt/', glob.glob('pcmanfm-qt/lxqt/*')),
