64bc15eb873f7056ff77227da71dcb628007372b
ayhanyalcinsoy <ayhanyalcinsoy@gmail.com>
2015-02-18 11:26:46 +0200

    -a

--- pisilinux-default-settings-lxqt/setup.py
+++ pisilinux-default-settings-lxqt/setup.py
@@ -5,11 +5,11 @@ import glob
 from distutils.core import setup
 
 datas = [
-    ('/usr/share/lxqt/themes/ambiance', glob.glob('ambiance/*')),
-    ('/usr/share/lxqt/themes/frost', glob.glob('frost/*')),
-    ('/usr/share/lxqt/themes/dark', glob.glob('dark/*')),
-    ('/usr/share/lxqt/themes/light', glob.glob('light/*')),
-    ('/usr/share/lxqt/themes/kde-plasma', glob.glob('kde-plasma/*')),
+    ('/usr/share/lxqt/themes/Ambiance', glob.glob('Ambiance/*')),
+    ('/usr/share/lxqt/themes/Frost', glob.glob('Frost/*')),
+    ('/usr/share/lxqt/themes/Dark', glob.glob('Dark/*')),
+    ('/usr/share/lxqt/themes/Light', glob.glob('Light/*')),
+    ('/usr/share/lxqt/themes/Kde-plasma', glob.glob('Kde-plasma/*')),
     ('/usr/share/lxdm/themes/lxdm-pisilinux-theme', glob.glob('lxdm/login.png')),
     ('/etc/lxqt/', glob.glob('lxqt/*')),
     ('/etc/pcmanfm-qt/lxqt/', glob.glob('pcmanfm-qt/lxqt/*')),
@@ -17,6 +17,6 @@ datas = [
 
 setup(
     name = 'pisilinux-default-settings-lxqt',
-    version = '0.1',
+    version = '0.2',
     data_files = datas,
     )
