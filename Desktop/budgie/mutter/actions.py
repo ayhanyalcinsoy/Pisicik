70d8242a89767d61737feab5e228f2e7619bfded
ayhanyalcinsoy <ayhanyalcinsoy@gmail.com>
2015-03-01 04:30:03 +0200

    mutter version bump

--- Desktop/budgie/mutter/actions.py
+++ Desktop/budgie/mutter/actions.py
@@ -5,15 +5,18 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    #autotools.autoreconf("-vfi")
+    autotools.autoreconf("-fi")
     autotools.configure("--disable-static \
+                         --disable-schemas-compile \
+                         --enable-compile-warnings=minimum\
                          --enable-introspection")
 
-    #pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make()
@@ -21,4 +24,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "NEWS", "README")
+    pisitools.dodoc("COPYING*", "NEWS")
