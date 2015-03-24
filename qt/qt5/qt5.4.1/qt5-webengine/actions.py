e3b1ec288e1b09ccc7f5db39ba52b07132b9dc86
ayhanyalcinsoy <ayhanyalcinsoy@gmail.com>
2015-03-25 00:07:52 +0200

    qt5.4.1

---
+++ qt/qt5/qt5.4.1/qt5-webengine/actions.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import qt5
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.system ("qmake-qt5 qtwebengine.pro")
+    autotools.rawConfigure ("-opensource \
+                             -eglfs \
+                             -opengl es2 \
+                             -xcb \
+                             -no-pch \
+                             -dbus-linked \
+                             -icu \
+                             -cups \
+                             -nis \
+                             -widgets \
+                             -gui \
+                             -qt-xcb \
+                             -openssl-linked \
+                             -system-libjpeg \
+                             -system-libpng \
+                             -system-zlib \
+                             -largefile \
+                             -c++11 \
+                             -shared \
+                             -no-static \
+                             -confirm-license \
+                             -release \
+                             -prefix /usr \
+                             -archdatadir /usr/lib/qt5 \
+                             -datadir /usr/share/qt5 \
+                             -docdir /usr/share/doc/qt5 \
+                             -sysconfdir /etc/xdg \
+                             -nomake tests \
+                             -xkb-config-root /usr/share/X11/xkb \
+                             -no-warnings-are-errors \
+                             -no-use-gold-linker \
+                             -examplesdir /usr/lib/qt5/examples")
+def build():
+    qt5.make()
+
+def install():
+    qt5.install("INSTALL_ROOT=%s" % get.installDIR())
+
+    #I hope qtchooser will manage this issue
+    for bin in shelltools.ls("%s/usr/lib/qt5/bin" % get.installDIR()):
+        pisitools.dosym("/usr/lib/qt5/bin/%s" % bin, "/usr/bin/%s-qt5" % bin)
+
+    pisitools.insinto("/usr/share/licenses/qt5-webengine/", "LGPL_EXCEPTION.txt")
