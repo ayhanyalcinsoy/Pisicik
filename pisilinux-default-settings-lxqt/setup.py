2a8fc6b03a301440c55d2263d8a25d07eaa077f0
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2015-08-04 12:44:54 +0300

    pisilinux-default-settings-lxqt:fixed

--- pisilinux-default-settings-lxqt/setup.py
+++ pisilinux-default-settings-lxqt/setup.py
@@ -10,11 +10,10 @@ datas = [
     ('/usr/share/lxqt/themes/Dark', glob.glob('Dark/*')),
     ('/usr/share/lxqt/themes/Light', glob.glob('Light/*')),
     ('/usr/share/lxqt/themes/Kde-plasma', glob.glob('Kde-plasma/*')),
-    ('/usr/share/lxdm/themes/lxdm-pisilinux-theme', glob.glob('lxdm/login.png')),
     ('/etc/lxqt/', glob.glob('lxqt/*')),
     ('/etc/pcmanfm-qt/lxqt/', glob.glob('pcmanfm-qt/lxqt/*')),
     ]
-
+    #('/usr/share/lxdm/themes/lxdm-pisilinux-theme', glob.glob('lxdm/login.png')),
 setup(
     name = 'pisilinux-default-settings-lxqt',
     version = '0.2',
