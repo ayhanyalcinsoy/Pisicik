3b37fa1cc9146dac0df7d6de2146940a900679a3
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2015-12-15 17:41:14 +0200

    lxqt:v.bump

--- Desktop/lxqt/base/lxqt-sudo/actions.py
+++ Desktop/lxqt/base/lxqt-sudo/actions.py
@@ -5,18 +5,14 @@
 
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import get
-from pisi.actionsapi import pisitools
+#from pisi.actionsapi import pisitools
 
 
 def setup():
-    cmaketools.configure("-DCMAKE_BUILD_TYPE=release \
-                          -DCMAKE_INSTALL_PREFIX=/usr \
-                          -DCMAKE_INSTALL_LIBDIR=/usr/lib")#\
-                          #-DUSE_QT5=ON")
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=release", installPrefix="/usr")
 
 def build():
     cmaketools.make()
 
 def install():
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("AUTHORS", "COPYING")
